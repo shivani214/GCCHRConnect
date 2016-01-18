5edc8a943bdd3094d5b86048ad7f90ac48e71e95
Shivani Gupta <helloshivani21apr@gmail.com>
2016-01-18 20:59:16 +0530

    Started Tags management
    Code for Tag management begun

---
+++ GCCHRConnect/GCCHRMachinery/DataAccessLayer/MongoDb/Contacts management/TagDB.cs
@@ -0,0 +1,19 @@
+﻿using GCCHRMachinery.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCCHRMachinery.DataAccessLayer.MongoDb
+{
+    class TagDB
+    {
+        public string CreateTag(Tag tag)
+        {
+            MongoClient client = new MongoClient();
+
+        }
+    }
+}
