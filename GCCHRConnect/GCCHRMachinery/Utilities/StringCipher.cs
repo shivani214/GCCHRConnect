1176d2a7faf49f97c16afa04dde2be8d131761af
Gaurang Gupta <gaurangfgupta@gmail.com>
2016-03-03 20:45:19 +0530

    Documentation of StringCipher
    Few tweaks too

--- GCCHRConnect/GCCHRMachinery/Utilities/StringCipher.cs
+++ GCCHRConnect/GCCHRMachinery/Utilities/StringCipher.cs
@@ -3,9 +3,20 @@ using System.Text;
 using System.Security.Cryptography;
 using System.IO;
 using System.Linq;
+using System.Security;
 
 namespace Utilities
 {
+    /// <summary>
+    /// This class provides encryption and decryption capabilities.
+    /// Encryption steps:-
+    /// 1. Any sensitive information should not be held in memory in the form of <see cref="string"/>. Hence it is recommended to convert it to <see cref="SecureString"/>immediately after it is received from input. <see cref="ToInsecureString(SecureString)"/> does this for you.
+    /// 2. Actual encryption is then performed by <see cref="Encrypt(SecureString, string)"/>.
+    /// 
+    /// Decryption steps:-
+    /// 1. Follow reverse steps of Encryption. The encrypted value must first be decrypted by <see cref="Decrypt(string, string)"/>
+    /// 2. The decrypted <see cref="SecureString"/> must be hopped around and its conversion to <see cref="string"/> by <see cref="ToInsecureString(SecureString)"/>must be delayed as much as possible
+    /// </summary>
     public static class StringCipher
     {
         // This constant is used to determine the keysize of the encryption algorithm in bits.
@@ -17,11 +28,12 @@ namespace Utilities
         /// <summary>
         /// Encrypts the provided string
         /// </summary>
-        /// <param name="plainText">The string to encrypt</param>
+        /// <param name="securedString">The string to encrypt</param>
         /// <param name="passPhrase">The password to use for encryption and decryption</param>
-        /// <returns></returns>
-        public static string Encrypt(string plainText, string passPhrase)
+        /// <returns>Encrypted string</returns>
+        public static string Encrypt(SecureString securedString, string passPhrase)
         {
+            string plainText = ToInsecureString(securedString);
             // Salt and IV is randomly generated each time, but is preprended to encrypted cipher text
             // so that the same Salt and IV values can be used when decrypting.  
             var saltStringBytes = Generate256BitsOfRandomEntropy();
@@ -62,8 +74,8 @@ namespace Utilities
         /// </summary>
         /// <param name="cipherText">The string to decrypt</param>
         /// <param name="passPhrase">The password provided during encryption</param>
-        /// <returns></returns>
-        public static string Decrypt(string cipherText, string passPhrase)
+        /// <returns>A secure string of the decrypted value</returns>
+        public static SecureString Decrypt(string cipherText, string passPhrase)
         {
             // Get the complete stream of bytes that represent:
             // [32 bytes of Salt] + [32 bytes of IV] + [n bytes of CipherText]
@@ -93,7 +105,8 @@ namespace Utilities
                                 var decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
                                 memoryStream.Close();
                                 cryptoStream.Close();
-                                return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
+                                SecureString securedString =ToSecureString(Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount));
+                                return securedString;
                             }
                         }
                     }
@@ -110,6 +123,42 @@ namespace Utilities
                 rngCsp.GetBytes(randomBytes);
             }
             return randomBytes;
+        }
+
+        /// <summary>
+        /// Converts a <see cref="string"/> to <see cref="SecureString"/>
+        /// </summary>
+        /// <param name="input">The plain string</param>
+        /// <returns>Secure string</returns>
+        public static SecureString ToSecureString(string input)
+        {
+            SecureString secure = new SecureString();
+            foreach (char c in input)
+            {
+                secure.AppendChar(c);
+            }
+            secure.MakeReadOnly();
+            return secure;
+        }
+
+        /// <summary>
+        /// Converts a <see cref="SecureString" to <see cref="string"/>/>
+        /// </summary>
+        /// <param name="input">The secure string</param>
+        /// <returns>Plain string</returns>
+        public static string ToInsecureString(SecureString input)
+        {
+            string returnValue = string.Empty;
+            IntPtr ptr = System.Runtime.InteropServices.Marshal.SecureStringToBSTR(input);
+            try
+            {
+                returnValue = System.Runtime.InteropServices.Marshal.PtrToStringBSTR(ptr);
+            }
+            finally
+            {
+                System.Runtime.InteropServices.Marshal.ZeroFreeBSTR(ptr);
+            }
+            return returnValue;
         }
     }
 }
